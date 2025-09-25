8f50af06e9e33e1c259875d5ca447c67c70b5f37
berradogruer <bediaberradogruer@gmail.com>
2025-09-25 15:59:10 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -175,7 +175,7 @@ export default function Home() {
             {/* Bir YouTube Shorts linkinden sadece ID'yi al ve aşağıya koy.
                Örn: https://youtube.com/shorts/ABC123  → ID: ABC123  → /embed/ABC123 */}
             <iframe
-              src="https://www.youtube.com/watch?v=LWjKo6E_uDc"  /* ← Kendi Shorts ID’n ile değiştir */
+              src="https://www.youtube.com/shorts/a-FJZ1NLjeg"  /* ← Kendi Shorts ID’n ile değiştir */
               title="Web geliştirme shorts"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
